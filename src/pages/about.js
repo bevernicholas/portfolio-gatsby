65053180ed1c9b7d558a70830254d7ccbea6929e
Nick Bever <nicholasbever@Nicholass-MacBook-Air-2.local>
2021-10-08 08:48:21 -0700

    completed about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,20 @@ import '../styles/_about.scss';
 
 const About = () => (
   <Layout>
-    <div>About</div>
+    <div className="about">
+      <div className="about__header">
+        <h1 className="about__header-text">Nicholas Bever</h1>
+        <div className="about__header-underline"></div>
+      </div>
+      <div className="about__description">
+        <p className="about__description-text">
+          Hello! My name is Nicholas Bever, and I am Software Engineer located in Seattle, WA.  I am passionate about creating dynamic user experiences, full scale applications in React, and REST APIs in Node.js, C#, and Python.
+        </p>
+        <p className="about__description-text">
+          My interest in software development begain in 2017 when I began teaching myself Python to better work with large data sets as a business associate.  Fast-forward to today and my curiosity has taken me far beyond my initial interest of manipulating large data sets using Python.  As a Software Engineer at Stackline I have created and contributed to numerous data pipelines in Node.js, .NET Core, and Spark, created full scale applications using React, developed REST APIs using Express.js and ASP.NET Core, and leveraged a number of AWS technoligies such as Lambda, ECS, DynamoDB, and EMR.
+        </p>
+      </div>
+    </div>
   </Layout>
 );
 
